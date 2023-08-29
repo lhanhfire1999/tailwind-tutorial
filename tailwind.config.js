678823ef145154c6eee48a629a70aaecbc2a4b1b
Anh Le <lhanhfire1999@gmail.com>
2023-08-29 23:57:08 +0700

    feat(hero-section): design ui/ux

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/images/hero.jpg')",
+      },
+    },
+  },
   plugins: [require("./plugins/openVariant")],
 };
