f4446a0be75719628e6d74b2210da4a21bd806f9
Anh Le <lhanhfire1999@gmail.com>
2023-08-30 18:21:29 +0700

    feat(hero-section): wave animation for logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/images/hero.jpg')",
       },
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
     },
   },
-  plugins: [require("./plugins/openVariant")],
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+  ],
 };
