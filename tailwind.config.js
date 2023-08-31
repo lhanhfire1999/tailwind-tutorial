170325ad61c042026ea16bf8bc2342d312608ea0
Anh Le <lhanhfire1999@gmail.com>
2023-08-31 23:50:51 +0700

    feat(headliners-section): add default heading css tag and bg-band

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/images/hero.jpg')",
+        band1: "url('/src/assets/images/band1.jpg')",
+        band2: "url('/src/assets/images/band2.jpg')",
+        band3: "url('/src/assets/images/band3.jpg')",
+        band4: "url('/src/assets/images/band4.jpg')",
+        band5: "url('/src/assets/images/band5.jpg')",
+        band6: "url('/src/assets/images/band6.jpg')",
+        band7: "url('/src/assets/images/band7.jpg')",
       },
       keyframes: {
         wavey: {
